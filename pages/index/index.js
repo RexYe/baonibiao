5d9876139b44d7fb04129661e3115fffc4ba3afa
ShawnYe <894450343@qq.com>
2017-09-20 21:21:59 +0800

    增加红包种类

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,11 +1,24 @@
 //index.js
 //发送红包页面
 var app = getApp()
+var jine={
+    menu_name:'金额（元）',
+    id:'je',
+    placeholder:'填写金额',
+    input_type:'digit',
+    confirm_type:'next',
+    maxlength:4
+  }
 Page({
   data: {
-    inputValue_kl:'',
-    inputValue_je:'',
-    inputValue_sl:'',
+    inputTxt:[
+      '',
+      '',
+      ''
+      // inputValue_kl:'',
+      // inputValue_je:'',
+      // inputValue_sl:'',
+    ],
     //服务费
     charge_money:'0.00',
     //按钮内的文本
@@ -14,91 +27,214 @@ Page({
     //是否显示警告栏
     warningShow:false,
     //用户余额
-    available_balance:0.00,
-    red_packet_input:[
+    available_balance:2.00,
+    //付款总金额,单位为分
+    cash:0,
+    choose_type_img:'./img/红包.png',
+    choose_type_text:'飙高音',
+    choose_type_div_if:false,
+    red_packet_input_type:'0',
+    hongbao_type:[
       {
-        menu_name:'设置口令',
-        id:'kl',
-        placeholder:'请输入口令',
-        input_type:'text',
-        confirm_type:'next',
-        maxlength:8,
-        tips:'小伙伴们说对口令即可获得随机红包 ',
-        tips_src:'./img/问号.png'
+        type:'飙高音',
+        imgsrc:'./img/红包.png'
       },
       {
-        menu_name:'金额（元）',
-        id:'je',
-        placeholder:'填写金额',
-        input_type:'digit',
-        confirm_type:'next',
-        maxlength:4
+        type:'唱红包',
+        imgsrc:'./img/红包.png'
       },
       {
-        menu_name:'数量（个）',
-        id:'sl',
-        placeholder:'填写数量',
-        input_type:'number',
-        maxlength:2
+        type:'提问红包',
+        imgsrc:'./img/红包.png'
+      },
+      {
+        type:'悬赏红包',
+        imgsrc:'./img/红包.png'
       }
+    ],
+    red_packet_input:[
+      // {
+      //   menu_name:'设置口令',
+      //   id:'kl',
+      //   placeholder:'请输入口令',
+      //   input_type:'text',
+      //   confirm_type:'next',
+      //   maxlength:8,
+      //   tips:'小伙伴们说对口令即可获得随机红包 ',
+      //   tips_src:'./img/问号.png'
+      // },
+      [
+        jine,
+        {
+          menu_name:'人数',
+          id:'rs',
+          placeholder:'填写人数',
+          input_type:'number',
+          maxlength:2
+        }
+      ],
+      [
+        {
+          menu_name:'选择歌词',
+          placeholder:'请输入歌词',
+          id:'geci',
+          maxlength:8,
+        },
+        jine,
+        {
+          menu_name:'数量',
+          id:'sl',
+          placeholder:'填写数量',
+          input_type:'number',
+          maxlength:2
+        }
+      ],
+      [
+        {
+          menu_name:'填写问题',
+          placeholder:'请输入问题',
+          id:'wenti',
+          id:'wenti',
+          maxlength:8,
+        },
+        jine,
+        {
+          menu_name:'数量',
+          id:'sl',
+          placeholder:'填写数量',
+          input_type:'number',
+          maxlength:2
+        }
+      ],
+      [
+        {
+          menu_name:'填写悬赏问题',
+          placeholder:'请输入悬赏问题',
+          id:'xs_wenti',
+          maxlength:8,
+        },
+        jine,
+        {
+          menu_name:'数量',
+          id:'sl',
+          placeholder:'填写数量',
+          input_type:'number',
+          maxlength:2
+        }
+      ]
     ],
     userInfo: {}
   },
   //事件处理函数
+  choose_type_div: function(){
+      const t = this;
+      t.setData({
+        choose_type_div_if:true
+      })
+  },
+  choose_type_done: function(e){
+    const t = this;
+    t.data.choose_type_text={'0':'飙红包','1':'唱红包','3':'猜红包','4':'提问红包'}[e.currentTarget.dataset.id] || '飙红包';
+    t.setData({
+      choose_type_div_if:false,
+      choose_type_text:t.data.choose_type_text,
+      red_packet_input_type:e.currentTarget.dataset.id,
+      inputTxt:''
+    })
+  },
   build_red_packet: function() {
-    wx.navigateTo({
-      url: '../hongbao/hongbao'
-    })
-    // const t = this
-    // //将时间戳从number转成string
-    // // let timestamp = (Date.parse(new Date())/1000).toString();
-    //调起微信支付
-    wx.requestPayment({
-      //均从后台获取
-      'timeStamp': '',
-      'nonceStr': '',
-      'package': '',
-      'signType': 'MD5',
-      'paySign': '',
-      'success':function(res){
-        wx.showToast({
-            title: '支付成功',
-            icon: 'success',
-            duration: 2000
-        })
-      },
-      'fail':function(res){},
-      'complete':function(res){}
-    })
-    // //点击支付前需判断是否符合支付条件
-    // let pre_money = Number(t.data.inputValue_je)/Number(t.data.inputValue_sl)
-    // if(pre_money>=1 && t.data.inputValue_je<=10){
-    //   t.setData({
-    //     warning:''
-    //   })
-    // }
-    // if(this.data.warning.length>0) {
-    //   wx.showModal({
-    //     title: '提示',
-    //     content: this.data.warning,
-    //     showCancel: false,
-    //   })
-    // }
-    // console.log(this.data);
+        const t = this;
+        // wx.navigateTo({
+        //   url: '../hongbao/hongbao'
+        // })
+
+        //点击支付前需判断是否符合支付条件
+        let pre_money = Number(t.data.inputValue_je)/Number(t.data.inputValue_sl)
+        if(pre_money>=1 && t.data.inputValue_je<=10){
+            t.setData({
+              warning:''
+            })
+        }
+        if(this.data.warning.length>0) {
+            wx.showModal({
+              title: '提示',
+              content: this.data.warning,
+              showCancel: false,
+            })
+        }
+        if(this.data.warning.length === 0){
+          //如余额大于等于支付金额，则无需调用微信支付。
+          if(t.data.cash <= t.data.available_balance){
+
+          }
+          //如余额小于需支付金额，则调用微信支付，并且支付的金额为抵消余额后的金额
+          else{
+
+          }
+            // console.log(this.data);
+            // 将时间戳从number转成string
+            // let timestamp = (Date.parse(new Date())/1000).toString();
+            // wx.request({
+            //       url: `${app.G.REQPREFIX}/api/wx/payment`,
+            //       method: 'GET',
+            //       data: {
+            //         cash:t.data.cash,   //订单金额
+            //         openid: self.data.openid
+            //       },
+            //       header: {
+            //           'content-type': 'application/json'
+            //       },
+            //       success: function(res) {
+            //         //调起微信支付
+            //         wx.requestPayment({
+            //           'timeStamp': timestamp,
+            //           'nonceStr': '',
+            //           'package': 'prepay_id='+res.data.prepay_id,
+            //           'signType': 'MD5',
+            //           'paySign': res.data._paySignjs,
+            //           'success':function(res){
+            //             wx.showToast({
+            //                 title: '支付成功',
+            //                 icon: 'success',
+            //                 duration: 2000
+            //             })
+            //           },
+            //           'fail':function(res){
+            //             wx.showToast({
+            //                 title: '支付失败',
+            //                 icon: 'loading',
+            //                 duration: 2000
+            //             })
+            //           },
+            //           'complete':function(res){}
+            //         })
+            //       },
+            //       fail: function(err) {
+            //           console.log(err)
+            //       }
+            //         })
+        }
   },
   how_to_use: function() {
-    console.log('how to use');
+    wx.navigateTo({
+      url: '../problems/problems'
+    })
   },
   bindKeyInput: function(e) {
+    console.log(e);
     const t = this
+
     //口令
     if(e.target.id === 'kl'){
-      t.setData({
-        inputValue_kl: e.detail.value
-      })
+      // t.data.inputTxt[0]= e.detail.value
+      // t.setData({
+      //   inputTxt: t.data.inputTxt
+      // })
     }
+
     //红包金额
     if(e.target.id === 'je') {
+      t.data.inputTxt[1]= e.detail.value
       //红包金额为空时,0或者未填
       if(e.detail.value == false) {
         t.setData({
@@ -138,18 +274,23 @@ Page({
         }
       }
       //服务费为4%，保留两位小数
-      let charge_money = (Number(e.detail.value)/25).toFixed(2)
+      let charge_money = (Number(e.detail.value)/16.6).toFixed(2)
       //需要支付的总额=服务费+红包金额
-      let all_money = Number(charge_money)+Number(e.detail.value)
+      let all_money = Number(charge_money)+Number(e.detail.value)-Number(t.data.available_balance)
       if(isNaN(all_money)){
         all_money = 0
       }
+      t.data.inputTxt[0]= e.detail.value
       //当计算出总额后，生成红包的按钮文本改成 ‘还需支付**元’
       if(all_money !== 0){
         t.setData({
+          // inputTxt: t.data.inputTxt,
           inputValue_je: Number(e.detail.value),
           charge_money: charge_money,
-          build_buttonValue: '还需支付'+all_money+'元'
+          build_buttonValue: '还需支付'+all_money+'元',
+          cash: all_money*100
+        },function () {
+            console.log(t.data.inputTxt);
         })
       }
       else{
